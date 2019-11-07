f6d69d821a63ef93f3d1ca804432ddb26136bc14
GyD4n13L <32672899+GyD4n13L@users.noreply.github.com>
2019-11-07 17:24:20 +0100

    Update deluged_install.sh

--- deluged_install.sh
+++ deluged_install.sh
@@ -3,6 +3,9 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install deluged -y
+sudo apt-get install mc -y
 sudo apt-get install deluge-console -y
 
 deluged sudo pkill deluged
+cp ~/.config/deluge/auth ~/.config/deluge/auth.old
+mcedit ~/.config/deluge/auth
